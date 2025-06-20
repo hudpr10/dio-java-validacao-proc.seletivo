28db8ac6b4e41facbab7c82e13fab8049eacab48
Hudson Reis <hudsonpr09@gmail.com>
2025-06-20 14:39:39 -0300

    analisando salario

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        salaryAnalyze();
+    }
+
+    public static void salaryAnalyze() {
+        System.out.println("\n--- Processo Seletivo ---\n");
+
+        double baseSalary = 2000.00;
+
+        System.out.print("Digite o salário pretendido para a vaga: ");
+        double desiredSalary = input.nextDouble();
+
+        if(baseSalary > desiredSalary) {
+            System.out.println("Ligar para o candidato.");
+        } else if(baseSalary == desiredSalary) {
+            System.out.println("Ligar para o candidato com contra proposta.");
+        } else {
+            System.out.println("Aguardando resultado demais candidatos.");
+        }
     }
 }
