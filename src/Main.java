6f8abcc1531d777267f03dff92badba3be270091
Hudson Reis <hudsonpr09@gmail.com>
2025-06-20 15:02:38 -0300

    selecionando candidatos

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,19 @@
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Main {
     public static Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        salaryAnalyze();
+        candidateSelection();
     }
 
-    public static void salaryAnalyze() {
+    public static void salaryAnalyze(double desiredSalary) {
         System.out.println("\n--- Processo Seletivo ---\n");
 
         double baseSalary = 2000.00;
 
-        System.out.print("Digite o salário pretendido para a vaga: ");
-        double desiredSalary = input.nextDouble();
+        System.out.print("Salário desejado: " + desiredSalary);
 
         if(baseSalary > desiredSalary) {
             System.out.println("Ligar para o candidato.");
@@ -22,5 +22,34 @@ public class Main {
         } else {
             System.out.println("Aguardando resultado demais candidatos.");
         }
+    }
+
+    public static double randomSalary() {
+        return ThreadLocalRandom.current().nextDouble(1800, 2200);
+    }
+
+    public static void candidateSelection() {
+        String[] candidates = {"Felipe", "Marcia", "Julia", "Paulo", "Augusto", "Monica", "Fabricio", "Mirela", "Daniela", "Jorge"};
+        int selectedCandidates = 0;
+        int candidatesIndex = 0;
+
+        double baseSalary = 2000;
+
+        while(selectedCandidates < 5 && candidatesIndex < candidates.length) {
+            String candidate = candidates[candidatesIndex];
+            double desiredSalary = randomSalary();
+
+            System.out.printf("O candidato %s solicitou R$ %s \n", candidate, desiredSalary);
+            if(baseSalary >= desiredSalary) {
+                selectedCandidates++;
+                System.out.printf("%s foi selecionado para a vaga.\n", candidate);
+            } else {
+                System.out.printf("%s não atende os requisitos.\n", candidate);
+            }
+
+            candidatesIndex++;
+        }
+
+        System.out.println("\nTotal de candidatos selecionados: " + selectedCandidates);
     }
 }
